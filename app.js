1d4a22e69c008c0267b1a5c58d960dfa7ade907c
umarbinhaniel <mandeepsg.m@gmail.com>
2021-12-30 20:37:29 -0500

    connect to DB

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const path = require('path');
+
+dotenv.config({ path: './.env' });
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+
+app.set('view engine', 'hbs');
 
 db.connect((error) => {
     if (error) {
@@ -18,7 +29,8 @@ db.connect((error) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("<h1> Home Page</h1>")
+    //res.send("<h1> Home Page</h1>")
+    res.render("index");
 });
 
 app.listen(5000, () => {
